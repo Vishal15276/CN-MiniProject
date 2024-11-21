ca99771c9bb0264b8a38237b24f2cf28942a1083
aaminaa04 <125854574+aaminaa04@users.noreply.github.com>
2024-11-21 09:37:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 8080;
 httpServer.listen(PORT, () => {
-  console.log(`Server is running on http://127.0.0.1:${PORT}`);
+  console.log(`Server  running on http://127.0.0.1:${PORT}`);
 });
